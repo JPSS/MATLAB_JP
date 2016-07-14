adbe240c43afbd78f98a9112b7c98ed9660edf68
JPSS <filip@JeanPhiiMac2015.dhcp.e14.ph.tum.de>
2016-07-14 19:14:02 +0200

    fix generate_parameter_set_indices.m to include iterating over structure nr

--- TOOLBOX_JP/cluster analysis/generate_parameter_set_indices.m
+++ TOOLBOX_JP/cluster analysis/generate_parameter_set_indices.m
@@ -1,15 +1,28 @@
 %% this generates a cell array with the parameter sets of a given simulation run, and their respective parameter set index
 
+% array{1} = [5];                        %loop rate exponents - arrayDuplicats
+% array{2} = [-2.44];                    %constant dG for loop rate factor - arrayExponents
+% array{3} = [-0.002579978];             %constant dG for non unique (double) good crossover formation - arrayDGLoop
+% array{4} = [0.0077, 0.0088, 0.01, 0.0111, 0.0122];%multiplicator double crossover premium - arrayMultiplicator
+% array{5} = [12.17222222, 16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
+% array{6} = [-8.33, -14.52222222];    %stacking energy - arrayDGBasestack
+% array{7} = [-2.534, 0];                 %dangling ends energy - arrayDGDangle
+% array{8} = [50, 55];                %temperature - arrayTemperatures
+% array{9} = [20, 7, 4];                 %minimum random match length - arrayMinLength
+% array{10} = [10000];                   %update step for forced loop rate update - arrayUpdateStep
+% array{11} = [3, 11, 01]               %structure nr
+
 array{1} = [5];                        %loop rate exponents - arrayDuplicats
 array{2} = [-2.44];                    %constant dG for loop rate factor - arrayExponents
 array{3} = [-0.002579978];             %constant dG for non unique (double) good crossover formation - arrayDGLoop
 array{4} = [0.0077, 0.0088, 0.01, 0.0111, 0.0122];%multiplicator double crossover premium - arrayMultiplicator
-array{5} = [12.17222222, 16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
-array{6} = [-8.33, -14.52222222];    %stacking energy - arrayDGBasestack
-array{7} = [-2.534, 0];                 %dangling ends energy - arrayDGDangle
+array{5} = [16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
+array{6} = [-14.52222222];    %stacking energy - arrayDGBasestack
+array{7} = [0, -0.33, -0.66, -1, -1.33, -1.66, -2, -2.25, -2.534];                 %dangling ends energy - arrayDGDangle
 array{8} = [50, 55];                %temperature - arrayTemperatures
 array{9} = [20, 7, 4];                 %minimum random match length - arrayMinLength
 array{10} = [10000];                   %update step for forced loop rate update - arrayUpdateStep
+array{11} = [3, 11, 01]               %structure nr
 
 nr_parameters = length(array);
 
@@ -23,9 +36,9 @@ results={};
 running = 1;
 while running
     %print parameter values to results
-    results{current_parameter_combination + 1,1} = current_parameter_combination;
+    results{1, current_parameter_combination + 1} = current_parameter_combination;
     for level = 1:nr_parameters
-        results{current_parameter_combination + 1, level + 1} = array{ level }( current_positions( level ) );
+        results{level + 1, current_parameter_combination + 1} = array{ level }( current_positions( level ) );
     end
     
     %advance index parameter combination
