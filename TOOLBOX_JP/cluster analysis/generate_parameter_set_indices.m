71cdb864c8d081b1012e60296da3ff57bfea58dd
Jean-Philippe Sobczak <>
2018-12-11 11:10:12 +0100

    add function to generate parameter indices for simulation submit scripts

--- TOOLBOX_JP/cluster analysis/generate_parameter_set_indices.m
+++ TOOLBOX_JP/cluster analysis/generate_parameter_set_indices.m
@@ -6,6 +6,18 @@
 % array{4} = [0.0077, 0.0088, 0.01, 0.0111, 0.0122];%multiplicator double crossover premium - arrayMultiplicator
 % array{5} = [12.17222222, 16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
 % array{6} = [-8.33, -14.52222222];    %stacking energy - arrayDGBasestack
+% array{7} = [0.0, -0.33, -0.66, -1.0, -1.33, -1.66, -2.0, -2.25, -2.534];                 %dangling ends energy - arrayDGDangle
+% array{8} = [50, 55];                %temperature - arrayTemperatures
+% array{9} = [20, 7, 4];                 %minimum random match length - arrayMinLength
+% array{10} = [10000];                   %update step for forced loop rate update - arrayUpdateStep
+% array{11} = [3, 11, 01]               %structure nr
+
+% array{1} = [5];                        %loop rate exponents - arrayDuplicats
+% array{2} = [-2.44];                    %constant dG for loop rate factor - arrayExponents
+% array{3} = [-0.002579978];             %constant dG for non unique (double) good crossover formation - arrayDGLoop
+% array{4} = [0.0, 0.0066, 0.0133, 0.02];%multiplicator double crossover premium - arrayMultiplicator
+% array{5} = [12.17222222, 16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
+% array{6} = [-8.33, -14.52222222];    %stacking energy - arrayDGBasestack
 % array{7} = [-2.534, 0];                 %dangling ends energy - arrayDGDangle
 % array{8} = [50, 55];                %temperature - arrayTemperatures
 % array{9} = [20, 7, 4];                 %minimum random match length - arrayMinLength
@@ -15,14 +27,14 @@
 array{1} = [5];                        %loop rate exponents - arrayDuplicats
 array{2} = [-2.44];                    %constant dG for loop rate factor - arrayExponents
 array{3} = [-0.002579978];             %constant dG for non unique (double) good crossover formation - arrayDGLoop
-array{4} = [0.0077, 0.0088, 0.01, 0.0111, 0.0122];%multiplicator double crossover premium - arrayMultiplicator
+array{4} = [0.000];%multiplicator double crossover premium - arrayMultiplicator
 array{5} = [16.15921667]; %energy penalty touching staple overhangs - arrayDGOverlap
 array{6} = [-14.52222222];    %stacking energy - arrayDGBasestack
-array{7} = [0, -0.33, -0.66, -1, -1.33, -1.66, -2, -2.25, -2.534];                 %dangling ends energy - arrayDGDangle
-array{8} = [50, 55];                %temperature - arrayTemperatures
-array{9} = [20, 7, 4];                 %minimum random match length - arrayMinLength
+array{7} = [0.0];                 %dangling ends energy - arrayDGDangle
+array{8} = [40,42,44,46,48,50,52,54,56];                %temperature - arrayTemperatures
+array{9} = [4];                 %minimum random match length - arrayMinLength
 array{10} = [10000];                   %update step for forced loop rate update - arrayUpdateStep
-array{11} = [3, 11, 01]               %structure nr
+array{11} = [0,1,2,3,4,5,6,7,8,11,13,14,16,18]               %structure nr
 
 nr_parameters = length(array);
 
@@ -31,14 +43,14 @@ current_positions = ones(nr_parameters,1);
 current_parameter_combination = 0;
 
 %output result
-results={};
+parameter_set={};
 
 running = 1;
 while running
     %print parameter values to results
-    results{1, current_parameter_combination + 1} = current_parameter_combination;
+    parameter_set{1, current_parameter_combination + 1} = current_parameter_combination;
     for level = 1:nr_parameters
-        results{level + 1, current_parameter_combination + 1} = array{ level }( current_positions( level ) );
+        parameter_set{level + 1, current_parameter_combination + 1} = array{ level }( current_positions( level ) );
     end
     
     %advance index parameter combination
